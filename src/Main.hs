aae30faded6d03ce7e3e517599eae953e98ee19c
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2014-04-27 22:57:19 +0200

    added GPL-3 Stuff to all source files.

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,26 @@
 {-|
 Module      : Main
 Description : grammata-Script Main-Module
-Copyright   : (c) Sascha Rechenberger, 2014
-License     : GPL-3
 Maintainer  : sascha.rechenberger@uni-ulm.de
 Stability   : experimental
 Portability : portable
+Copyright   : (c) Sascha Rechenberger, 2014
+License     : GPL-3
+
+This file is part of grammata.
+
+grammata is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+grammata is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with grammata. If not, see <http://www.gnu.org/licenses/>.
 -}
 
 module Main 
