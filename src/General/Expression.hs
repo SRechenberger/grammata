8fc4559010dc20976d632c64ac0dd08200837075
Sascha Rechenberger LinuxPool <sascha.rechenberger@uni-ulm.de>
2014-05-14 15:42:15 +0200

    UNCOMPILABLE grammata version 0.1.7.2
    CHANGES
        - Extended abstract Expression type.
    
    TODO
        - Use this type.

--- src/General/Expression.hs
+++ src/General/Expression.hs
@@ -25,22 +25,76 @@ along with grammata. If not, see <http://www.gnu.org/licenses/>.
 
 module General.Expression
 (
-    -- * Expression type.
-    Expression (Variable, Constant, Binary, Unary, Application)
+    -- * @Expression@ type.
+    Expression (Variable, Constant, Binary, Unary, Application),
+    
+    -- * Classes to evaluate @Expression@s.
+    Identifier (load), Value (checkUnary, checkBinary, applyable, apply), FailableMonad (failEval)
 )
 where
 
-   -- |Arithmetical expressions
+    import Control.Applicative ((<$>),(<*>))
+    import Data.List (intercalate)
+
+    -- |Classifies a type as an identifier usable to evaluate expressions.
+    class Eq i => Identifier i where
+        -- |Yields a value from a given @Identifier@.
+        load :: (FailableMonad m, Value v) => i -> m v
+    
+    -- |Classifies a monad (and functor) as failable in case of an evaluation fail.
+    class (Monad m, Functor m) => FailableMonad m where
+        -- |Fails the evaluation.
+        failEval :: String -> m a
+        
+    -- |Classifies a value usable to evaluate expressions.
+    class Value v where
+        -- |Checks whether two values are compatible to be evaluated with a binary operator.
+        checkBinary :: v -> v -> Bool
+        -- |Checks whether a unary operator can be applied to the value.
+        checkUnary :: v -> Bool
+        -- |Checks whether a value is an appliable function.
+        applyable :: v -> Bool
+        -- |Applies a function to a given list of parameters.
+        apply :: (FailableMonad m, Identifier i)=> v -> [Expression i v] -> m v
+    
+    -- |Evaluates an @Expression@.
+    eval :: (FailableMonad m, Identifier i, Value v) => Expression i v  -- ^ Expression to evaluate.
+                                                     -> m v             -- ^ Result in a failable Monad.
+    eval (Variable id) = load id
+    eval (Constant val) = return val
+    eval (Binary f e1 e2) = do 
+        val1 <- eval e1
+        val2 <- eval e2
+        if val1 `checkBinary` val2 
+            then return $ f val1 val2
+            else failEval $ "Incompatible values."
+    eval (Unary f e) = do
+        val <- eval e
+        if checkUnary val 
+            then return $ f val
+            else failEval $ "Cannot do unary application."
+    eval (Application id exprs) = do
+        f <- load id
+        if applyable f 
+            then apply f exprs
+            else failEval $ "Cannot do application."
+    
+    -- |Arithmetical expressions
     data Expression identifier value =
+        -- |A variable, named with an identifier.
           Variable identifier
+        -- |A constant value.
         | Constant value
-        | Binary (value -> value -> value) Expression Expression
-        | Unary (value -> value) Expression
-        | Application identifier [Expression] 
+        -- |A binary operator.
+        | Binary (value -> value -> value) (Expression identifier value) (Expression identifier value)
+        -- |A unary operator.
+        | Unary (value -> value) (Expression identifier value)
+        -- |A function application.
+        | Application identifier [Expression identifier value] 
 
     instance (Show identifier, Show value) => Show (Expression identifier value) where
-        show (Variable id) = "id (" ++ id ++ ")"
+        show (Variable id) = "id (" ++ show id ++ ")"
         show (Constant c)  = "const (" ++ show c ++ ")"
         show (Binary _ e1 e2) = "(" ++ show e1 ++ " ° " ++ show e2 ++ ")"
         show (Unary _ e) = "(*" ++ show e ++ ")"
-        show (Application fid es) = fid ++ "(" ++ intercalate "," (map show es) ++ ")"
+        show (Application fid es) = show fid ++ "(" ++ intercalate "," (map show es) ++ ")"
