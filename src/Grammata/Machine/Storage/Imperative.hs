8851bb8ca8a980db18d3fd57bb129a2f9b816093
Sascha Rechenberger LinuxPool <sascha.rechenberger@uni-ulm.de>
2014-07-09 15:55:10 +0200

    BEGAN REIMPLEMENTATION grammata 0.2.0.0
    	modified:   src/Grammata/Machine/Storage/Imperative.hs

--- src/Grammata/Machine/Storage/Imperative.hs
+++ src/Grammata/Machine/Storage/Imperative.hs
@@ -28,3 +28,33 @@ module Grammata.Machine.Storage.Imperative
 
 )
 where
+
+    import Prelude hiding (lookup)
+    import Data.Map (Map, fromList, lookup)
+
+    type Frame ident vartype = Map ident vartype 
+    
+    type Stack ident vartype = [Frame ident vartype]
+    
+    data IStorage ident vartype = IStorage {
+        global :: Frame ident vartype,
+        locals :: Stack ident vartype
+        } 
+        
+    enter :: Ord ident => [(ident, vartype)] -> Stack ident vartype -> Stack ident vartype
+    enter vars stack = fromList vars : stack
+    
+    read :: (Show ident, Monad m, Ord ident) => ident -> Stack ident vartype -> m vartype
+    read ident (frame:_) = case lookup ident frame of
+        Nothing -> fail "IMPERATIVE STACK ERROR undefined identifier " ++ show ident
+        Just x  -> return x
+        
+    write :: (Show ident, Monad m, Ord ident) => ident -> vartype -> Stack ident vartype -> m (Stack ident vartype)
+    write ident datum (frame:frames) = if member ident frame 
+        then return $ adjust (const datum) ident frame : frames
+       
+    leave :: Stack ident vartype -> m (Stack ident vartype)
+    leave (_:stack) = return stack
+    
+    
+    
