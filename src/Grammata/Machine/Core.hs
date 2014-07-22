f3b7aa200127e86fa41c9b6aa0836f855f01eccd
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2014-07-22 23:53:27 +0200

    REIMPLEMENT grammata 0.2.0.1
    worked at Grammata.Machine.Core
    
    modified:   src/Grammata/Machine/Core.hs

--- src/Grammata/Machine/Core.hs
+++ src/Grammata/Machine/Core.hs
@@ -9,6 +9,7 @@ where
     {- EXPRESSION CORE -}
     data Expression ident basic = 
           Constant basic
+        | Symbol ident
         | Function ([basic] -> basic) [(Expression ident basic)]  
     
     class Monad m => FromBasic m basic | m -> basic where
@@ -50,6 +51,7 @@ where
         | App (Lambda ident basic) [Lambda ident basic]
         | Letrec [(ident,Lambda ident basic)] (Lambda ident basic)
         | Cond (Lambda ident basic) (Lambda ident basic) (Lambda ident basic)
+        | Bound basic 
     
     class Monad m => CoreLambdaMonad m ident basic | m -> ident basic where
         getBasicFromLambda :: Lambda ident basic -> m basic
@@ -59,28 +61,57 @@ where
         rewrite            :: [(basic, Lambda ident basic)] -> m ()
         
 
-    bind :: [(ident, basic)] -> Lambda ident basic -> m (Lambda ident basic)
-    bind = undefined
+    bind :: (Monad m, Eq ident)
+        => [(ident, basic)] 
+        -> Lambda ident basic 
+        -> m (Lambda ident basic)
+    bind bindings (Abs params e) = return $ (if length params > length bindings then Abs params else id) (foldr bind' e bindings)
+        where
+            bind' (ident, bsc) expr = case expr of
+                Basic expr    -> Basic $ bindInExpr (ident,bsc) expr
+                Abs ids e     -> if ident `elem` ids then Abs ids e else Abs ids $ bind' (ident,bsc) e 
+                App e1 e2     -> App (bind' (ident,bsc) e1) (map (bind' (ident,bsc)) e2)
+                Letrec defs e -> Letrec (map (\(id,e) -> (id, bind' (ident,bsc) e)) defs) (bind' (ident,bsc) e)
+                Cond c e1 e2  -> Cond (bind' (ident,bsc) c) (bind' (ident,bsc) e1) (bind' (ident,bsc) e2)
+                others        -> others
 
-    free :: Lambda ident basic -> m [ident]
-    free = undefined
+            bindInExpr (ident,basic) expr = case expr of
+                Symbol s -> if s == ident then Constant basic else Symbol s
+                Function f args -> Function f $ map (bindInExpr (ident,basic)) args 
+                others -> others
 
-    bound :: Lambda ident basic -> m [ident]
-    bound = undefined
 
-    evalCoreLambda :: CoreLambdaMonad m ident basic => (Lambda ident basic) -> m basic
-    evalCoreLambda = undefined
+    bound :: (Monad m) 
+        => Lambda ident basic 
+        -> m [ident]
+    bound (Abs ids _) = return ids 
+    bound _           = return []
 
-    reduceCoreLambda :: (FromBasic m basic, CoreLambdaMonad m ident basic) => Lambda ident basic -> m (Lambda ident basic)
-    reduceCoreLambda (Basic e) = evalCoreExpression e >>= getLambdaFromBasic
+    evalCoreLambda :: (FromBasic m basic, CoreLambdaMonad m ident basic, Eq ident) 
+        => Lambda ident basic 
+        -> m (Lambda ident basic)
+    evalCoreLambda expr = do
+        expr' <- reduceCoreLambda expr 
+        if isRedex expr' 
+            then evalCoreLambda expr'
+            else return expr'
+
+    reduceCoreLambda :: (FromBasic m basic, CoreLambdaMonad m ident basic, Eq ident) 
+        => Lambda ident basic 
+        -> m (Lambda ident basic)
+    reduceCoreLambda (Bound ptr) = do
+        bsc <- getLambdaFromBasic ptr >>= evalCoreLambda
+        rewrite [(ptr,bsc)] 
+        return bsc
+
     reduceCoreLambda (App e args) = do 
-        ptrs <- new args
         vars <- bound e
+        ptrs <- new $ take (length vars) args
         bind (vars `zip` ptrs) e
 
     reduceCoreLambda (Letrec defs e) = do
         ptrs <- alloc (length defs) 
-        vars <- free e
+        vars <- return . fst . unzip $ defs
         defs' <- mapM (bind (zip vars ptrs)) (snd . unzip $ defs) >>= return . zip ptrs
         rewrite defs'
         bind (zip vars ptrs) e
@@ -91,8 +122,10 @@ where
             then thenE
             else elseE
 
-    isRedex :: Lambda ident basic -> Bool
-    isRedex (Basic _) = True
+    isRedex :: () 
+        => Lambda ident basic 
+        -> Bool
+    isRedex (Basic (Function _ _)) = True
     isRedex (App _ _) = True
     isRedex (Letrec _ _) = True
     isRedex (Cond _ _ _) = True
